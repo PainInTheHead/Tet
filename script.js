715a32913803e3d04b06a01828b3089fa7866094
IlyaBragin <ibragin@sfedu.ru>
2023-12-11 10:13:49 +0300

    fix and upgrade view

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ const arrFigName = ["T", "Q", "I", "Z", "S", "J", "L"];
 let total = 0;
 let timeUp = 0.7;
 let inputTime;
-let timeOutId
+let timeOutId;
 
 let record = total;
-window.onload = function () {
+
+document.addEventListener("DOMContentLoaded", (event) => {
   let savedScrore = localStorage.getItem("record");
   if (!savedScrore) {
     document.getElementById("record").innerText = "0";
   }
   document.getElementById("record").innerText = savedScrore;
-};
+});
 
 function changeDifficults() {
   const selectedElement = document.getElementById("difficult");
@@ -86,7 +87,9 @@ class Tetris {
   constructor() {
     this.gameArea;
     this.tetobj;
+    this.tetArr = [];
     this.isGameOver = false;
+    this.render = false;
     this.generationGameArea();
     this.generationObj();
   }
@@ -95,13 +98,18 @@ class Tetris {
     const name = getRandomIndex(arrFigName);
     const matrix = allMatrxOfFig[name];
     const column = 5;
+    const nextFigure = getRandomIndex(arrFigName);
+    const nextMatrix = allMatrxOfFig[nextFigure];
     const row = -1;
     this.tetobj = {
       name,
       matrix,
       column,
       row,
+      nextFigure,
+      nextMatrix,
     };
+    this.tetArr.push(nextFigure);
   }
 
   generationGameArea() {
@@ -137,15 +145,48 @@ class Tetris {
   }
 
   rotate() {
-    const rotatedMatrix = rotateMatrix(this.tetobj.matrix);
-    this.tetobj.matrix = rotatedMatrix;
+    let matrixSize;
+    let matrix;
+    let name;
+    if (this.render === true) {
+      name = this.tetArr[tetris.tetArr.length - 2];
+      matrix = allMatrxOfFig[name];
+      matrixSize = matrix.length;
+      const rotatedMatrix = rotateMatrix(allMatrxOfFig[name]);
+      if (!this.checkOutBorders()) {
+        allMatrxOfFig[name] = allMatrxOfFig[name];
+      } else {
+        allMatrxOfFig[name] = rotatedMatrix;
+      }
+    } else {
+      name = this.tetobj.name;
+      matrixSize = this.tetobj.matrix.length;
+      matrix = this.tetobj.matrix;
+      const rotatedMatrix = rotateMatrix(this.tetobj.matrix);
+      this.tetobj.matrix = rotatedMatrix;
+
+      if (!this.checkOutBorders()) {
+        this.tetobj.matrix = matrix
+      }
+    }
   }
 
   checkOutBorders() {
-    const matrixSize = this.tetobj.matrix.length;
+    let matrixSize;
+    let matrix;
+    let name;
+    if (this.render === true) {
+      name = this.tetArr[tetris.tetArr.length - 2];
+      matrix = allMatrxOfFig[name];
+      matrixSize = matrix.length;
+    } else {
+      name = this.tetobj.name;
+      matrixSize = this.tetobj.matrix.length;
+      matrix = this.tetobj.matrix;
+    }
     for (let row = 0; row < matrixSize; row++) {
       for (let column = 0; column < matrixSize; column++) {
-        if (!this.tetobj.matrix[row][column]) continue;
+        if (!matrix[row][column]) continue;
         if (this.isTruePosition(row, column)) return false;
         if (this.touching(row, column)) return false;
       }
@@ -162,22 +203,40 @@ class Tetris {
   }
 
   stateTetris() {
-    const matrixSize = this.tetobj.matrix.length;
+    let matrixSize;
+    let matrix;
+    let name;
+    if (this.render === true) {
+      name = this.tetArr[tetris.tetArr.length - 2];
+      matrix = allMatrxOfFig[name];
+      matrixSize = matrix.length;
+    } else {
+      name = this.tetobj.name;
+      matrixSize = this.tetobj.matrix.length;
+      matrix = this.tetobj.matrix;
+    }
     for (let row = 0; row < matrixSize; row++) {
       for (let column = 0; column < matrixSize; column++) {
-        if (!this.tetobj.matrix[row][column]) continue;
+        if (!matrix[row][column]) continue;
         if (this.outsideTop(row)) {
           this.isGameOver = true;
+          if (tetris.isGameOver) {
+            gameOver();
+            setNewRecord();
+          }
           return;
         }
 
         this.gameArea[this.tetobj.row + row][this.tetobj.column + column] =
-          this.tetobj.name;
+          name;
       }
     }
+    this.render = true;
     this.filtersOfRows();
     this.generationObj();
-    this.down();
+    if (!this.isGameOver) {
+      this.down();
+    }
   }
 
   outsideTop(row) {
@@ -223,6 +282,7 @@ const tetris = new Tetris();
 console.log(tetris.gameArea);
 
 const cells = document.querySelectorAll(".grid>div");
+const cellsNext = document.querySelectorAll(".gridNext>div");
 
 keysPosition();
 
@@ -233,19 +293,14 @@ function keysPosition() {
 function moveDown() {
   tetris.down();
   paintInt();
-  stopDown()
+  stopDown();
   startDown();
-
-  if (tetris.isGameOver) {
-    gameOver();
-    setNewRecord();
-  }
 }
 
 function gameOver() {
-  stopDown()
+  alert("GAME OVER");
+  stopDown();
   document.removeEventListener("keydown", swicthKey);
-  alert("Саня погиб");
 }
 
 function setNewRecord() {
@@ -255,13 +310,14 @@ function setNewRecord() {
     if (record < total) {
       record = total;
       localStorage.setItem("record", record.toString());
-      document.getElementById("record").innerText = localStorage.getItem('record');
+      document.getElementById("record").innerText =
+        localStorage.getItem("record");
     }
   } else if (!newRecord) {
     let record = parseInt(newRecord);
     record = total;
     localStorage.setItem("record", record.toString());
-    document.getElementById("record").innerText = ocalStorage.getItem('record');
+    document.getElementById("record").innerText = ocalStorage.getItem("record");
   }
 }
 
@@ -274,7 +330,7 @@ function startDown() {
 }
 
 function stopDown() {
-  clearTimeout(timeOutId)
+  clearTimeout(timeOutId);
 }
 
 setTimeout(() => {
@@ -285,7 +341,6 @@ setTimeout(() => {
     }, inputTime - 500);
   };
 }, 10000);
-
 
 function moveLeft() {
   tetris.left();
@@ -336,22 +391,58 @@ function swicthKey(e) {
 
 function paintInt() {
   cells.forEach((cell) => cell.removeAttribute("class"));
+  cellsNext.forEach((cell) => cell.removeAttribute("class"));
+  renderNext();
   paintArea();
   paint();
 }
 
-function paint() {
-  const matrixSize = tetris.tetobj.matrix.length;
-  const name = tetris.tetobj.name;
+function renderNext() {
+  const name = tetris.tetobj.nextFigure;
+  const matrixSize = tetris.tetobj.nextMatrix.length;
   for (let row = 0; row < matrixSize; row++) {
-    for (let col = 0; col < matrixSize; col++) {
-      if (!tetris.tetobj.matrix[row][col]) continue;
-      if (tetris.tetobj.matrix[row][col] < 0) continue;
-      const cellIndex = getIndexHtml(
-        row + tetris.tetobj.row,
-        col + tetris.tetobj.column
-      );
-      cells[cellIndex].classList.add(name);
+    for (let column = 0; column < matrixSize; column++) {
+      if (!tetris.tetobj.nextMatrix[row][column]) continue;
+      const cellNIndex = getNextIndexHtml(row, column);
+      cellsNext[cellNIndex].classList.add(name);
+    }
+  }
+}
+
+function paint() {
+  let matrixSize;
+  let name;
+  let matrix;
+  if (tetris.render === true) {
+    name = tetris.tetArr[tetris.tetArr.length - 2];
+    matrix = allMatrxOfFig[name];
+    matrixSize = matrix.length;
+    for (let row = 0; row < matrixSize; row++) {
+      for (let col = 0; col < matrixSize; col++) {
+        if (!matrix[row][col]) continue;
+        if (matrix[row][col] < 0) continue;
+        const cellIndex = getIndexHtml(
+          row + tetris.tetobj.row,
+          col + tetris.tetobj.column
+        );
+        cells[cellIndex].classList.add(name);
+      }
+    }
+  } else {
+    matrixSize = tetris.tetobj.matrix.length;
+    name = tetris.tetobj.name;
+    matrix = tetris.tetobj.matrix;
+
+    for (let row = 0; row < matrixSize; row++) {
+      for (let col = 0; col < matrixSize; col++) {
+        if (!matrix[row][col]) continue;
+        if (matrix[row][col] < 0) continue;
+        const cellIndex = getIndexHtml(
+          row + tetris.tetobj.row,
+          col + tetris.tetobj.column
+        );
+        cells[cellIndex].classList.add(name);
+      }
     }
   }
 }
@@ -369,4 +460,8 @@ function paintArea() {
 
 function getIndexHtml(r, c) {
   return r * 10 + c;
+}
+
+function getNextIndexHtml(r, c) {
+  return r * 4 + c;
 }
